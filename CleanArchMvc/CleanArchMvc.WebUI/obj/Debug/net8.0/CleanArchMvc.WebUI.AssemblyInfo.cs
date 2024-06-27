b40d5e00f3c1b242669c8dbbf4308d9ef887fd0d
wendel filipe <wendelfilipe1997@hotmail.com>
2024-06-26 21:52:33 -0300

    update main

--- CleanArchMvc/CleanArchMvc.WebUI/obj/Debug/net8.0/CleanArchMvc.WebUI.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/obj/Debug/net8.0/CleanArchMvc.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0a5128e8743aad9b22c966783619bb78e99c96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d71c5ac639d8f7f95aef2b768008302f650425c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
