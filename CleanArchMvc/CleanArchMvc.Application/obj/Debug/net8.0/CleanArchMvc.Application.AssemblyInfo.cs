4cd2a0e9821a603677a1e4da629912b2c9ed084b
wendel filipe <wendelfilipe1997@hotmail.com>
2024-07-14 21:08:10 -0300

    update main

--- CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea81ab7bbf23bb1200cbd14b1d329271e2805fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66910faa4be610fe8c3051d93f5ae887d3a0a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
