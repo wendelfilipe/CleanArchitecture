fa0b9198ffc4bde8ea7953c1bb775848133f90f8
wendel filipe <wendelfilipe1997@hotmail.com>
2024-07-17 20:13:27 -0300

    update main

--- CleanArchMvc/CleanArchMvc.Infra.Ioc/obj/Debug/net8.0/CleanArchMvc.Infra.Ioc.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Ioc/obj/Debug/net8.0/CleanArchMvc.Infra.Ioc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24434f79edf6f121ba844d00b43dfd22b0d077ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d5c2b78b62e91c488331bfd4df6432d9a62406")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Infra.Ioc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
