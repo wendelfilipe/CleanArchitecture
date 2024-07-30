5e8f270e003d87f98b51fd1e25e2f8e70f5d8a9d
wendel filipe <wendelfilipe1997@hotmail.com>
2024-07-29 21:01:21 -0300

    update

--- CleanArchMvc/CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
+++ CleanArchMvc/CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2040d69bc99e4c34a0ff4ded545e1b6197eb2cb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20617d8785732b579b8dfbcda8a6157132a3bfa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
